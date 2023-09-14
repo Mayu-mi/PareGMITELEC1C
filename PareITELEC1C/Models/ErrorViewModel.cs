0bd06a340b1fba17ad0961d56cf0fb1dbb2c5fc9
Goldie <114350089+Mayu-mi@users.noreply.github.com>
2023-09-15 00:16:29 +0800

    Delete PareITELEC1C directory

--- PareITELEC1C/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PareITELEC1C.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
